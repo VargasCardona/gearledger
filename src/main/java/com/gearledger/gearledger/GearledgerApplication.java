9d51a9533cff2a8288dbd52eb586506d3a4d5829
Nicolas <vargascardona12@gmail.com>
2025-04-12 01:16:52 -0500

    chore: initial commit

---
+++ src/main/java/com/gearledger/gearledger/GearledgerApplication.java
@@ -0,0 +1,13 @@
+package com.gearledger.gearledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearledgerApplication.class, args);
+	}
+
+}
